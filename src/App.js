eb3925174beb342552d9c4a486c15df53d929ebd
Morgan Peterson <morenalatina@Morgan-MBP.home>
2020-11-29 16:50:31 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
 import MichaelLogo from './MichaelLogo.jpeg';
-import './App.css';
+import React, { Component } from 'react';
+// import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Contact from './Components/Contact';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+class App extends Component  {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={MichaelLogo} className="App-logo" alt="logo" />
-        <h1 >
-          Under Construction
-        </h1>
-        <div>
-          <p>Phone: (704)-317-8081</p>
-          <p>Toll Free: 1(888)-928-3422</p>
-          <p>Fax: (704) 517-3249</p>
-        </div>
-      </header>
+    
+      <div className="App">
+       
+      
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+      <img
+        alt="logo"
+        src={MichaelLogo} 
+        width="60"
+        height="60"
+        className="d-inline-block align-top"
+      />{' '}
+      Michael Langston Peterson
+    </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href= "/Contact">Contact Us</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#Contact">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+      <Switch>
+              <Route exact path='/Contact' component={Contact} />
+                        <Route render={function () {
+                            return <p>Not found</p>
+                        }} />
+                   </Switch>
+          <header className="App-header"/>
+            
+            <h1 >
+              Under Construction
+            </h1>
+          
+          
+        
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/Components/Contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Contact</h1>
+            <div>
+              <p>Phone: (704)-317-8081</p>
+              <p>Toll Free: 1(888)-928-3422</p>
+              <p>Fax: (704) 517-3249</p>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default Contact
