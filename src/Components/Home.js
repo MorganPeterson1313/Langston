111ef180d380202087a5703f463d0e3df3558878
Morgan Peterson <morenalatina@Morgan-MBP.home>
2020-11-29 21:43:49 -0500

    added picture to homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,7 +14,17 @@ export class Home extends Component {
     render() {
         return (
             <div className= "Home">
+                
             <div style={{ display: "flex" , justifyContent: "center", marginBottom:"20%"}}>
+            <img
+                 
+                 alt="header Image"
+                 src="https://images.unsplash.com/photo-1532079563951-0c8a7dacddb3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80" 
+                 Width= "65%"
+                 height= "5%"
+                 className="d-inline-block align-top"
+                
+                />
             <Card style={{ width: '25rem'}}>
   <Card.Img variant="top" src={Michael} />
   <Card.Body>
