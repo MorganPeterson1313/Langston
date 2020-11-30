9b5c465b1f23a16615acead53146504c6091fa2c
Morgan Peterson <morenalatina@Morgan-MBP.home>
2020-11-30 16:11:20 -0500

    added form control fix

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -26,7 +26,7 @@ export class Contact extends Component {
     <div style={{alignContent: "center", margin: "2%"}}>
 					<form>
     
-  <Form name= "contact" method="POST" data-netlify="true"> 
+  <Form name= "contact" method="POST" data-netlify="true" onSubmit="submit"> 
   <Form.Group controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
     <Form.Control type="email" placeholder="Enter email" />
