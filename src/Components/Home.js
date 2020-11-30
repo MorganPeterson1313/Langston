4b4be51d34f5a0a301fc2292366ec5e7dfa1f203
Morgan Peterson <morenalatina@Morgan-MBP.home>
2020-11-29 22:25:40 -0500

    deleted height option in home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -21,7 +21,7 @@ export class Home extends Component {
                  alt="header Image"
                  src="https://images.unsplash.com/photo-1532079563951-0c8a7dacddb3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80" 
                  Width= "65%"
-                 height= "5%"
+                 
                  className="d-inline-block align-top"
                 
                 />
